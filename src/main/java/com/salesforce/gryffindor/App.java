eac2b747a82147c779b94fbce9b7c820b61c0801
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2018-02-06 12:32:58 -0500

    added settings

--- src/main/java/com/salesforce/gryffindor/App.java
+++ src/main/java/com/salesforce/gryffindor/App.java
@@ -2,10 +2,9 @@ package com.salesforce.gryffindor;
 
 /**
  * Hello world!
- *
  */
 public class App {
-	public static voidz main(String[] args) {
-		System.out.printn("Hello, World");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
