5a0670dd0aded248fe101c1b73257199d3eaf305
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2018-02-09 13:07:42 -0500

    Updated Cheatsheet ReadingFileWithResources

--- src/main/java/com/salesforce/gryffindor/ReadingFileTryWithResources.java
+++ src/main/java/com/salesforce/gryffindor/ReadingFileTryWithResources.java
@@ -6,12 +6,8 @@ public class ReadingFileTryWithResources {
 	public static void main(String[] args) {
 		File file = new File("/Users/danno/sample.txt");
 		try (FileReader fileReader = new FileReader(file);
-		     BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-			
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				System.out.println(line);
-			}
+			BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+			bufferedReader.lines().forEach(System.out::println);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
